7474f2d56dcaefa1962f812661c2dd7aada92409
M-Aqilaz <muhaaqil6002@gmail.com>
2025-04-16 11:03:31 +0700

    Aqil2

--- KELOMPOK/src/MainBank.java
+++ KELOMPOK/src/MainBank.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class MainBank {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ServiceTransaksi service = new ServiceTransaksi();
